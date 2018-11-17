462405b94644b1f18f30f7ec47f6eb24243f0496
Blessing Krofegha <bkrofegha@gmail.com>
2018-11-16 16:27:57 -0800

    updated cred

--- update_cred.php
+++ update_cred.php
@@ -1,14 +1,4 @@
-<?php
-/**
-* Boomchat
-*
-* @package Boomchat
-* @author www.myboomchat.com
-* @copyright 2015
-* @terms any use of this script without a legal license is prohibited
-* all the content of Boomchat is the propriety of BoomCoding and Cannot be 
-* used for another project.
-*/
+
 require_once("system/config.php");
 
 $me = $user['user_name'];
