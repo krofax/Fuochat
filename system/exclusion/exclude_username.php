90ee81012b92137e20f0d0889f670d928e63abf6
Blessing Krofegha <bkrofegha@gmail.com>
2018-11-16 16:48:13 -0800

    added more words

--- system/exclusion/exclude_username.php
+++ system/exclusion/exclude_username.php
@@ -2,4 +2,4 @@
 
 // at the line bellow you can insert between the " " all the word that you do not want to authorize to be used inside a username 
 
-$exclude_in_username = array( 'fuck', 'bitch' );
+$exclude_in_username = array( 'fuck', 'bitch', 'sex' , 'penis', 'pussy', 'satan' );
