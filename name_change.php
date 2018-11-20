f04ecaf710ee991aec20bc90e6db9a622cce77f1
Blessing Krofegha <bkrofegha@gmail.com>
2018-11-20 09:14:57 -0800

    included regexp

--- name_change.php
+++ name_change.php
@@ -1,14 +1,5 @@
 <?php
-/**
-* Boomchat
-*
-* @package Boomchat
-* @author www.myboomchat.com
-* @copyright 2015
-* @terms any use of this script without a legal license is prohibited
-* all the content of Boomchat is the propriety of BoomCoding and Cannot be 
-* used for another project.
-*/
+
 require_once("system/config.php");
 require_once("system/exclusion/exclude_username.php");
 
